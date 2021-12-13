69602ce14907432bb5a0db0d09befc3982393338
DersonDawson <andersonsmurf@gmail.com>
2021-12-12 23:47:17 -0300

    descobri o erro

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)* box,
+    y: Math.floor(Math.random() * 15 + 1)* box
+}
 
 function criarBG(){
     context.fillStyle = "darkblue";
-    context.fillRect(0, 0, 16 * box, 16 * box)
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
@@ -20,26 +24,56 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "yellow";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+
+}
 
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
-    let snakeY =snake[0].y;
+    let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+
+    }
+    else{
+      food.x = Math.floor(Math.random() * 15 + 1)* box;
+       food.y =  Math.floor(Math.random() * 15 + 1)* box;
+
+    }
+
+    
 
     let newHead = {
         x: snakeX,
         y: snakeY
     }
-    snakeY.unshift(newHead);
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);
